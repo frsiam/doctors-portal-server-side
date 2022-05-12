eae7262b644f4bc53ed3ba3b4a3c6907d624394c
frsiam <siam.frs@gmail.com>
2022-05-12 12:58:21 +0600

    add an get api for get all data from client side by sending a get request

--- index.js
+++ index.js
@@ -15,14 +15,23 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect(err => {
+async function run() {
     try {
-        const collection = client.db("test").collection("devices");
+        await client.connect();
+        const servicesCollection = client.db("doctors-portal").collection("services");
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
     }
     finally {
         // client.close();
     }
-});
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
