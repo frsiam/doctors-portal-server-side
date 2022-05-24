db0ce27c2f010e3e76a96d10420346a2b946add2
frsiam <siam.frs@gmail.com>
2022-05-24 13:06:06 +0600

    check the user role is admin or not from database

--- index.js
+++ index.js
@@ -49,14 +49,28 @@ async function run() {
             res.send(users);
         });
 
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updatedDoc = {
-                $set: { role: 'admin' }
-            };
-            const result = await userCollection.updateOne(filter, updatedDoc);
-            res.send(result);
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const email = req.params.email;
+                const filter = { email: email };
+                const updatedDoc = {
+                    $set: { role: 'admin' }
+                };
+                const result = await userCollection.updateOne(filter, updatedDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden access' })
+            }
         });
 
         app.put('/user/:email', async (req, res) => {
